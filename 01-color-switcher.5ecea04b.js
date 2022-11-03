433e4e55495b76190cf7215962c6e277d3d6a532
YevheniiaVier <107929873+YevheniiaVier@users.noreply.github.com>
2022-11-03 23:08:41 +0000

    Deploying to gh-pages from @ YevheniiaVier/goit-js-hw-09@1b69e40ef6ddd6febdc46bb112fc2b7dde0d57a7 🚀

---
+++ 01-color-switcher.5ecea04b.js
@@ -0,0 +1,2 @@
+const t={body:document.querySelector("body"),startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]")};let e=null;function n(){t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}t.startBtn.addEventListener("click",(function(){if(t.startBtn.disabled)return;t.startBtn.disabled=!0,e=setInterval((()=>{n(),console.log("GO!")}),1e3)})),t.stopBtn.addEventListener("click",(function(){clearInterval(e),t.startBtn.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.5ecea04b.js.map
